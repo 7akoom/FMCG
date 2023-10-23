79956dbfc62a1610f00dc91ba508f91e96a89fb7
Abdulrahman <dev.kazkaz@gmail.com>
2023-10-23 17:48:57 +0300

    Update AuthMiddleware.php

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -23,7 +23,7 @@ class AuthMiddleware
     public function handle(Request $request, Closure $next)
     {
 
-        Log::debug('headers', ['data' => $request->headers('citycode', 'username', 'password', 'source_type')]);
+        Log::debug('headers', ['data' => $request->header('citycode', 'username', 'password', 'source_type')]);
 
         $type = $request->header('source_type');
 
