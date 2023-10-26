5f07069d60bdbfaa1c0c1a31fabd7a8ae67b9fd9
Abdulrahman <dev.kazkaz@gmail.com>
2023-10-26 15:54:06 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'Login successful',
-            'data' => $salesMan
+            'data' => $salesMan[0]
         ]);
 
     }
